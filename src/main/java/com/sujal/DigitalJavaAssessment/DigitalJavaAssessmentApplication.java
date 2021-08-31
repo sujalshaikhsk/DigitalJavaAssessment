e7d0aec47880ab244bdcdfd03d55a2f3a3e65eca
Sujal Shaikh <sujalshaikhsk@gmail.com>
2021-08-31 09:03:55 +0530

    setting up the project

---
+++ src/main/java/com/sujal/DigitalJavaAssessment/DigitalJavaAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.sujal.DigitalJavaAssessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalJavaAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalJavaAssessmentApplication.class, args);
+	}
+
+}
